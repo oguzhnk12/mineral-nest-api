fa8eab707a604e10aba5fe8362e585aa320a203c
Oğuzhan Kaya <oguzhan_kobe@hotmail.com>
2025-06-14 01:51:28 +0300

    Kullanıcı haritalama işlemleri için yeni sınıflar eklendi
    Yeni `UserMappingProfile` ve `UserMappingService` sınıfları, `AutoMapper` kullanarak `UserCreateRequestDto` ve `AddressCreateRequestDto` nesnelerini sırasıyla `User` ve `Address` nesnelerine dönüştürmek için haritalama yapılandırmaları ve hizmetler sağlar. Bu değişiklik, kullanıcı ile ilgili verilerin daha etkili bir şekilde yönetilmesine olanak tanır.

---
+++ Application/Mapping/MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.UserFeatures;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapping.MappingProfiles
+{
+    public class UserMappingProfile: Profile
+    {
+        public UserMappingProfile() {
+            CreateMap<AddressCreateRequestDto, Address>();
+            CreateMap<UserCreateRequestDto, User>()
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address));
+        }
+
+    }
+}
---
+++ Application/Mapping/MappingServices/UserMappingService.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.UserFeatures;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapping.MappingServices
+{
+    internal class UserMappingService
+    {
+        private readonly IMapper _mapper;
+
+        public UserMappingService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public User MapToUser(UserCreateRequestDto dto)
+        {
+            return _mapper.Map<User>(dto);
+        }
+
+        public Address MapToAddress(AddressCreateRequestDto dto)
+        {
+            return _mapper.Map<Address>(dto);
+        }
+    }
+}
