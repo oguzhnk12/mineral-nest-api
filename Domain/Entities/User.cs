b210c72284163d4c42faf1ee8d24f8446b1cc1ee
Oğuzhan Kaya <oguzhan_kobe@hotmail.com>
2025-06-13 23:47:30 +0300

    User entity oluşturuldu

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enumerators;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required Role Role { get; set; }
+        public bool IsEmailVerified { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastLogin { get; set; }
+    }
+}
---
+++ Domain/Enumerators/Role.cs
@@ -0,0 +1,8 @@
+﻿    namespace Domain.Enumerators
+{
+    public enum Role
+    {
+        Admin = 1,
+        User = 2,
+    }
+}
