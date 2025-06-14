7e37d948e3fd93a2cb22ded1859bd35cb468033a
Oğuzhan Kaya <oguzhan_kobe@hotmail.com>
2025-06-14 13:09:46 +0300

    Hava durumu ve kimlik doğrulama kontrolörleri eklendi
    `WeatherForecastController` ile hava durumu tahminleri döndüren bir kontrolör tanımlandı. Hava durumu özetlerini içeren bir dizi ve logger kullanıldı. GET isteği için sunucu adresi güncellendi.
    
    `AuthController` eklendi; kullanıcı giriş işlemlerini yöneten bir `Login` metodu içeriyor.
    
    `DumbController` eklendi; basit bir yanıt döndüren yetkilendirilmiş bir kontrolör.
    
    `launchSettings.json` dosyasında uygulama URL'leri güncellendi.
    `WeatherForecast` sınıfı, tarih ve sıcaklık bilgilerini temsil ediyor.

---
+++ Presentation/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.UserFeatures;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private readonly IAuthService _service;
+
+        private readonly HttpClient _httpClient;
+
+        public AuthController(IAuthService service, HttpClient httpClient)
+        {
+            _service = service;
+            _httpClient = httpClient;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequestDto request)
+        {
+            var result = await _service.Login(request);
+            if (result != null)
+            {
+                return Ok(new ApiResponse<UserLoginResponseDto>(result, "Kullanıcı Başarıyla Oluşturuldu."));
+            }
+            return StatusCode(500, new ApiResponse<string?>(null, "Beklenmeyen Bir Hata Oluştu"));
+        }
+    }
+}
---
+++ Presentation/Controllers/DumbController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.UserFeatures;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class DumbController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetDumbData()
+        {
+            return Ok((new ApiResponse<string>("This is a dummy response", "Dummy data retrieved successfully.")));
+        }
+    }
+}
